5a9e967b7cb5a0f12c87c9d7a90c2979a703a8f9
Sowmiyadevi44 <sowmisaran2@gmail.com>
2017-06-07 18:37:32 +0530

    Update and rename world.java to word.java

---
+++ word.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class word
+{
+	public static void main(String args[])
+	{
+		int i;
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		for(i=0;i<5;i++)
+		System.out.println(s1);
+	}
+	}
